73fc3d2c024f4e723ef2b578b9d8dc419cc99275
loktionov129 <loktionov129@gmail.com>
2018-01-14 12:01:03 +0700

    [tests]: add NUnit

---
+++ WebApiServer.Tests/AccountTests.cs
@@ -0,0 +1,36 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AccountTests.cs" company="mpgp">
+//   Multiplayer Game Platform
+// </copyright>
+// <summary>
+//   Defines the AccountTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WebApiServer.Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// The account tests.
+    /// </summary>
+    public class AccountTests
+    {
+        /// <summary>
+        /// The setup.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// The test 1.
+        /// </summary>
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
