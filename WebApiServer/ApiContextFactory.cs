e2babf281e54bcefa3dfe1d2ea66c4105bd0f495
loktionov129 <loktionov129@gmail.com>
2018-01-26 00:26:17 +0700

    Add ApiContextFactory

---
+++ WebApiServer/ApiContextFactory.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ApiContextFactory.cs" company="mpgp">
+//   Multiplayer Game Platform
+// </copyright>
+// <summary>
+//   Defines the ApiContextFactory type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WebApiServer
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.EntityFrameworkCore.Infrastructure;
+    
+    public class ApiContextFactory : IDesignTimeDbContextFactory<Models.ApiContext>
+    {
+        public Models.ApiContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<Models.ApiContext>();
+            optionsBuilder.UseSqlite("Data Source=master.db");
+
+            return new Models.ApiContext(optionsBuilder.Options);
+        }
+    }
+}
